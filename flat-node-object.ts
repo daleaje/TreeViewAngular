21e5f2d1e8c3bd768afebd9a79f8f75f315efcd4
hidayat hasan (Dale) <weiwei45@hotmail.com>
2022-06-09 10:55:36 -0500

    Create flat-node-object.ts

---
+++ flat-node-object.ts
@@ -0,0 +1,9 @@
+/** Flat node with expandable and level info */
+export class FlatNodeObject{
+  expandable: boolean;
+  name: string;
+  level: number;
+  objectId: number;
+  path: string;
+  relativePath: string;
+}
